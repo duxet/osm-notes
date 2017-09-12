3d35cfa4312e3e3ff546260e4cd0d3f5e35a6ef4
duxet <duxetlg@gmail.com>
2017-09-12 21:37:41 +0200

    Add web frontend skeleton

---
+++ frontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div style="height: 100%">
+    <v-map :zoom=13 :center="[47.413220, -1.219482]">
+      <v-tilelayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></v-tilelayer>
+      <v-marker :lat-lng="[47.413220, -1.219482]"></v-marker>
+    </v-map>
+  </div>
+</template>
+
+<script>
+import L from 'leaflet'
+
+export default {
+  
+}
+</script>
+
+<style lang="scss">
+    @import "~bulma/sass/utilities/_all";
+
+    // Customization goes here
+
+    @import "~bulma";
+    @import "~buefy/src/scss/buefy";
+    @import "~leaflet/dist/leaflet.css";
+</style>
---
+++ frontend/webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            'scss': 'vue-style-loader!css-loader!sass-loader',
+            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+          }
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  },
+  devtool: '#eval-source-map'
+}
+
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
+}
